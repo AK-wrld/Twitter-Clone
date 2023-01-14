f89d330aa34ed59c35bf032d3723985c58d75777
Apurba Koley <btech.i1_44583@apsc.mait.ac.in>
2023-01-15 01:36:49 +0530

    Added functionality for user to upload or change their profile pic

--- models/User.js
+++ models/User.js
@@ -18,5 +18,9 @@ const UserSchema = new Schema({
         type: Date,
         default : Date.now
     },
+    userimage: {
+        type:String,
+        default:"https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
+    }
   });
   module.exports = mongoose.model('user',UserSchema)
